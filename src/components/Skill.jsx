c18c92cd039fb97bdda0831952b8c14bdb7e77cb
Killian0812 <ngcuong0812@gmail.com>
2025-05-22 17:10:38 +0700

    update skills

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,24 +1,40 @@
-import React from "react";
-
 import nodejs from '../assets/techs/nodejs.png'
 import springboot from '../assets/techs/springboot.png'
 import mysql from '../assets/techs/mysql.png'
 import redis from '../assets/techs/redis.png'
 import react from '../assets/techs/react.png'
-import redux from '../assets/techs/redux.png'
-import express from '../assets/techs/express.png';
-import tailwind from '../assets/techs/tailwind.png'
 import mongodb from '../assets/techs/mongodb.png'
 import firebase from '../assets/techs/firebase.png'
 import reactNative from '../assets/techs/reactnative.png'
 import git from '../assets/techs/git.png'
+import flutter from '../assets/techs/flutter.svg'
+import gcp from '../assets/techs/gcp.png'
+import tensorflow from '../assets/techs/tensorflow.png'
 
 const Skill = () => {
   const techs = [
     {
-      id: 1,
-      src: springboot,
-      title: "Spring Boot",
+      id: 5,
+      src: nodejs,
+      title: "Node.js",
+      style: "shadow-green-500",
+    },
+    {
+      id: 4,
+      src: react,
+      title: "React",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 13,
+      src: flutter,
+      title: "Flutter",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 7,
+      src: mongodb,
+      title: "MongoDB",
       style: "shadow-green-500",
     },
     {
@@ -34,46 +50,10 @@ const Skill = () => {
       style: "shadow-red-500",
     },
     {
-      id: 4,
-      src: react,
-      title: "React",
-      style: "shadow-blue-500",
-    },
-    {
-      id: 5,
-      src: nodejs,
-      title: "Node.js",
+      id: 1,
+      src: springboot,
+      title: "Spring Boot",
       style: "shadow-green-500",
-    },
-    {
-      id: 6,
-      src: express,
-      title: "Express.js",
-      style: "shadow-gray-500",
-    },
-    {
-      id: 7,
-      src: mongodb,
-      title: "MongoDB",
-      style: "shadow-green-500",
-    },
-    {
-      id: 8,
-      src: tailwind,
-      title: "TailwindCSS",
-      style: "shadow-blue-500",
-    },
-    {
-      id: 9,
-      src: redux,
-      title: "Redux",
-      style: "shadow-purple-500",
-    },
-    {
-      id: 10,
-      src: firebase,
-      title: "Firebase",
-      style: "shadow-yellow-500",
     },
     {
       id: 11,
@@ -86,6 +66,24 @@ const Skill = () => {
       src: git,
       title: "Git",
       style: "shadow-red-500",
+    },
+    {
+      id: 10,
+      src: firebase,
+      title: "Firebase",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 14,
+      src: gcp,
+      title: "Google Cloud",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 15,
+      src: tensorflow,
+      title: "TensorFlow",
+      style: "shadow-yellow-500",
     },
   ];
 
@@ -106,10 +104,10 @@ const Skill = () => {
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
-              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+              className={`pt-5 shadow-md hover:scale-105 duration-500 py-2 rounded-lg flex flex-col items-center justify-between h-40 ${style}`}
             >
               <img src={src} alt="" className="w-20 mx-auto" />
-              <p className="mt-4">{title}</p>
+              <p className="mb-1">{title}</p>
             </div>
           ))}
         </div>
